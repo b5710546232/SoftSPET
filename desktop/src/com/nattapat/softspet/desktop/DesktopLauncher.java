a582795be52e7ce527150d01f8102a71b4304dd9
nattapat sukpootanan <nattapat.su@ku.th>
2016-04-17 08:20:25 +0700

    init

---
+++ desktop/src/com/nattapat/softspet/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nattapat.softspet.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nattapat.softspet.SoftspetMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SoftspetMain(), config);
+	}
+}
