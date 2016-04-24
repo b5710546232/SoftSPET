104a3c742f028584c692cfd628decf62b7aa00e8
nattapat sukpootanan <nattapat.su@ku.th>
2016-04-25 00:29:55 +0700

    add button to foodscreen

---
+++ core/src/com/nattapat/softspet/screens/FoodScreen.java
@@ -0,0 +1,163 @@
+package com.nattapat.softspet.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.nattapat.softspet.SoftspetMain;
+import com.nattapat.softspet.gameworld.GameRenderer;
+import com.nattapat.softspet.gameworld.GameWorld;
+import com.nattapat.softspet.util.Assets;
+import com.nattapat.softspet.util.Constants;
+
+/**
+ * Created by nattapat on 4/6/2016 AD.
+ */
+public class FoodScreen implements Screen {
+    private static final String TAG = FoodScreen.class.getName();
+    private GameRenderer renderer;
+    private GameWorld world;
+    private Stage stage;
+    private float stateTime = 0;
+    private Image BG;
+    private ImageButton breadButton;
+    private ImageButton meatButton;
+    private Game game;
+    private final float POS_Y = 256;
+    private final float INITPOS_X = -64;
+    private final float FINALPOS_X = 360+64;
+    private final float MID_X = 116;
+    private int STATE = 0;
+    private boolean active = false;
+
+    public FoodScreen(Game game){
+    this.game = game;
+    }
+
+
+
+    @Override
+    public void show() {
+        stage = new Stage(new StretchViewport(Constants.VIEWPORT_WIDTH,
+                Constants.VIEWPORT_HEIGHT));
+        rebuildStage();
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    public void rebuildStage(){
+        Table layerBackground = buildBackgroundLayer();
+        Table layerMeatButton = buildMeatButton();
+        Table layerBreadButton = buildBreadButton();
+        stage.clear();
+        Stack stack = new Stack();
+        stack.setSize(Constants.VIEWPORT_WIDTH,
+                Constants.VIEWPORT_HEIGHT);
+        stage.addActor(stack);
+        stack.add(layerBackground);
+        stack.add(layerMeatButton);
+        stack.add(layerBreadButton);
+        breadButton.setPosition(MID_X, POS_Y + 64);
+        meatButton.setPosition(MID_X, POS_Y - 64);
+        initListener();
+    }
+
+    private void initListener() {
+        breadButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                ((SoftspetMain) game).getPlayScreen().getWorld().getPet().eat(10);
+                game.setScreen(((SoftspetMain) game).getPlayScreen());
+
+            }
+        });
+        meatButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                ((SoftspetMain) game).getPlayScreen().getWorld().getPet().eat(10);
+                game.setScreen(((SoftspetMain) game).getPlayScreen());
+
+            }
+        });
+    }
+
+    private Table buildBackgroundLayer(){
+        Table layer  = new Table();
+        BG = new Image(new TextureRegionDrawable(Assets.BG));
+        layer.add(BG);
+        return layer;
+    }
+
+    private Table buildMeatButton(){
+        Table layer  = new Table();
+        meatButton = new ImageButton(new TextureRegionDrawable(Assets.meat_textureRegion),
+                new TextureRegionDrawable( Assets.meat_textureRegion));
+        layer.addActor(meatButton);
+        return layer;
+    }
+
+    private Table buildBreadButton(){
+        Table layer  = new Table();
+        breadButton = new ImageButton(new TextureRegionDrawable(Assets.bread_texutreRegion),
+                new TextureRegionDrawable( Assets.bread_texutreRegion));
+        layer.addActor(breadButton);
+        return layer;
+    }
+
+    @Override
+    public void render (float deltaTime) {
+        Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(deltaTime);
+        stage.draw();
+        if(Gdx.input.justTouched()){
+            int x = Gdx.input.getX();
+            int y = Gdx.input.getY();
+            Vector3 inputs = new Vector3(x, y, 0);
+            stage.getCamera().unproject(inputs);
+//            Gdx.app.error("input : ", " y is " + inputs.y);
+//            Gdx.app.error("input : "," x is "+ inputs.x);
+            if((inputs.y<=180 || inputs.y>=420 ) || (inputs.x<=110 || inputs.x>=230))
+            game.setScreen(((SoftspetMain) game).getPlayScreen());
+
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width,height,true);
+
+    }
+
+    @Override
+    public void pause() {
+        Gdx.app.error(TAG, " pause ");
+
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+
+    }
+}
---
+++ core/src/com/nattapat/softspet/screens/PlayScreen.java
@@ -0,0 +1,186 @@
+package com.nattapat.softspet.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.nattapat.softspet.SoftspetMain;
+import com.nattapat.softspet.gameworld.GameRenderer;
+import com.nattapat.softspet.gameworld.GameWorld;
+import com.nattapat.softspet.util.Assets;
+import com.nattapat.softspet.util.Constants;
+
+/**
+ * Created by nattapat on 4/6/2016 AD.
+ */
+public class PlayScreen implements Screen {
+    private static final String TAG = PlayScreen.class.getName();
+    private GameRenderer renderer;
+    private GameWorld world;
+    private Stage stage;
+    private float stateTime = 0;
+    private SoftspetMain game;
+    private ImageButton foodButton;
+    private ImageButton medButton;
+    private ImageButton lightButton;
+    private ImageButton toiletButton;
+    private ImageButton miniGameButton;
+
+
+    public PlayScreen(Game game){
+        this.game = (SoftspetMain)game;
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+
+        float gameWidth = Constants.VIEWPORT_WIDTH;
+        float gameHeight = screenHeight/(screenWidth / gameWidth);
+        world = new GameWorld();
+        renderer = new GameRenderer(world,(int)gameHeight);
+    }
+
+    private void initButton() {
+        foodButton = new ImageButton(new TextureRegionDrawable(Assets.food_button_up),
+                new TextureRegionDrawable( Assets.food_button_down));
+        foodButton.setPosition(10, 30);
+        stage.addActor(foodButton);
+
+        medButton = new ImageButton(new TextureRegionDrawable(Assets.med_button_up),
+                new TextureRegionDrawable(Assets.med_button_down));
+        medButton.setPosition(79, 30);
+        stage.addActor(medButton);
+
+        lightButton = new ImageButton(new TextureRegionDrawable(Assets.light_button_up),
+                new TextureRegionDrawable(Assets.light_button_down));
+        lightButton.setPosition(148, 30);
+        stage.addActor(lightButton);
+
+        toiletButton = new ImageButton(new TextureRegionDrawable(Assets.toilet_button_up),
+                new TextureRegionDrawable(Assets.toilet_button_down));
+        toiletButton.setPosition(217, 30);
+        stage.addActor(toiletButton);
+        
+        miniGameButton = new ImageButton(new TextureRegionDrawable(Assets.minigame_button_up),
+                new TextureRegionDrawable(Assets.minigame_button_down));
+        miniGameButton.setPosition(286, 30);
+        stage.addActor(miniGameButton);
+
+        initListener();
+
+    }
+    public GameWorld getWorld() {
+        return world;
+    }
+
+    private void initListener() {
+        foodButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                if(world.getPet().isActive && !world.getPet().sleeping)
+                game.setScreen(((SoftspetMain) game).getFoodScreen());
+//                if(world.getPet().isActive && !world.getPet().sleeping)
+//                world.getPet().eat(10);
+            }
+        });
+
+        medButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                if(world.getPet().isActive && !world.getPet().sleeping){
+                    world.injectVaccine();
+                }
+
+            }
+        });
+
+        lightButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                if(world.getPet().isActive)
+                    world.switchLight();
+            }
+        });
+
+        toiletButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                if(world.getPet().isActive && !world.getPet().sleeping)
+                world.clean();
+            }
+        });
+
+        miniGameButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                if(world.getPet().isActive && !world.getPet().sleeping)
+                Gdx.app.error(TAG,"should play some game");
+            }
+        });
+
+    }
+
+
+    @Override
+    public void show() {
+        stage = new Stage(new StretchViewport(Constants.VIEWPORT_WIDTH,
+                Constants.VIEWPORT_HEIGHT));
+        stage.clear();
+        Gdx.input.setInputProcessor(stage);
+        initButton();
+    }
+
+    @Override
+    public void render(float delta) {
+        stateTime+=delta;
+        //Draw a black bg. This prevents flickering.
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        //// Clears the screen
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        world.update(delta);
+        renderer.render(delta,stateTime);
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        renderer.resize(width,height);
+        stage.getViewport().update(width, height, true);
+
+    }
+
+    @Override
+    public void pause() {
+        Gdx.app.error(TAG, " pause ");
+
+    }
+
+    @Override
+    public void resume() {
+        world.getPet().stateTime = 0;
+        Assets.instance.init(new AssetManager());
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        renderer.dispose();
+        stage.dispose();
+
+    }
+}
